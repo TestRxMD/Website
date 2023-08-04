4e3d3a5076e066aacd2b1ed459b7d6db94f1c607
Jacob Briggs <jdbriggs81@gmail.com>
2023-08-04 16:28:07 -0500

    removed config and patient info table

--- src/helper/check_rfill.js
+++ src/helper/check_rfill.js
@@ -1,6 +1,6 @@
-const PatientInfo=require('../models/patientInfoModel')
+// const PatientInfo=require('../models/patientInfoModel')
 
-const refillLeft=async(userId)=>{
-    const patient_info=await PatientInfo.findOne({where:{userId:userId}})
-    return patient_info.refill
-}
+// const refillLeft=async(userId)=>{
+//     const patient_info=await PatientInfo.findOne({where:{userId:userId}})
+//     return patient_info.refill
+// }
